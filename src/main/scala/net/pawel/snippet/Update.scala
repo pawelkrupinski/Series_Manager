97a418a5d1236008185a3e53e51f1a2b2a7faa30
Paweł Krupinski <pawel@krupinski.com>
2011-10-29 10:29:25 +0100

    Improving episode update process.

--- src/main/scala/net/pawel/snippet/Update.scala
+++ src/main/scala/net/pawel/snippet/Update.scala
@@ -11,18 +11,26 @@ class Update {
 
   def update {
     val series: List[Series] = Series.findAll
-    series.flatMap(updateSeries(_))
+    series.par.flatMap(updateSeries(_))
   }
 
   def updateSeries(series: Series): List[Episode] = {
     println("Updating " + series.name)
-    val existing_episodes = series.episodes.map(e => (e.episode_id.get, e)).toMap
+    val existing_episodes = series.episodes.map(_.key).zip(series.episodes).toMap
     val online_episodes = fetch_episodes(series)
 
     def updateEpisode(online_episode: Episode): Option[Episode] =
-      existing_episodes.get(online_episode.episode_id) match {
+      existing_episodes.get(online_episode.key) match {
         case Some(existing_episode) => existing_episode.update(online_episode)
-        case None => {online_episode.save; Some(online_episode)}
+        case None => {
+          online_episode.series.last_watched match {
+            case Some(episode) => if (episode > online_episode) online_episode.watched(true)
+          }
+
+          online_episode.save;
+
+          Some(online_episode)
+        }
       }
 
     online_episodes.flatMap(updateEpisode(_) match {
